f3b1f597d2af8bb4c0133146ed63ff122744e6f8
HassebJ <11besemjaved@seecs.edu.pk>
2016-02-25 22:07:36 +0500

    working query1

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose'),
     async = require('async'),
     extend = require('extend'),
     cm_data = mongoose.model('cm_data');
+var json2csv = require('json2csv');
+var fs = require('fs');
+
 
 module.exports = function(app) {
 
@@ -38,7 +41,7 @@ module.exports = function(app) {
 
                 fieldsToSearch._p_user = req.query.user;
 
-                async.forEachOf(periods, function (period, i, foreachcallback) {
+                async.forEachOfSeries(periods, function (period, i, foreachcallback) {
                     fieldsToSearch._created_at = {'$gte': new Date(period.start), '$lt': new Date(period.end)};
                     var fields = "_p_user longitude latitude battery model version accuracy confidence activity -_id";
                     var prevData = [];
@@ -81,8 +84,10 @@ module.exports = function(app) {
                 fieldsToSearch._p_user = {'$ne': req.query.user};
                 var results = [];
                 var maxDistance = parseInt(req.query.distance)/6371;
+                var sortQ = {};
+                sortQ[sort] = sortType;
 
-                async.forEachOf(initialusers, function (record, i, foreachcallback) {
+                async.forEachOfSeries(initialusers, function (record, i, foreachcallback) {
                     var user = record.data;
                     console.log("user : " + record.data);
                     var coords = [];
@@ -99,7 +104,7 @@ module.exports = function(app) {
                     cm_data.find(fieldsToSearch, fields,{
                             skip: skip,
                             limit: limit,
-                            //sort:sortQ
+                            sort:sortQ
                         },
                         function(err, data){
                             if (err){
@@ -111,13 +116,34 @@ module.exports = function(app) {
                             var current = [];
                             data.forEach(function(thisuser){
                                 console.log(JSON.stringify(thisuser));
-                                var usrObj= {queryUserId: user._p_user, queryUserLong: user.longitude, queryUserLat:user.latitude, queryUserBattery: user.battery, queryUserModel: user.model, queryUserVersion: user.version, queryUserAccuracy:user.accuracy, queryUserConfidence: user.confidence, queryUserActivity: user.activity  };
-                                var times = {start: record.start, end : record.end};
-                                extend(true, thisuser, usrObj, times);
+                                var usrObj = {
+                                    start: record.start,
+                                    end: record.end,
+                                    queryUserId: user._p_user,
+                                    queryUserLong: user.longitude,
+                                    queryUserLat: user.latitude,
+                                    queryUserBattery: user.battery,
+                                    queryUserModel: user.model,
+                                    queryUserVersion: user.version,
+                                    queryUserAccuracy: user.accuracy,
+                                    queryUserConfidence: user.confidence,
+                                    queryUserActivity: user.activity,
+                                    userId: thisuser._p_user,
+                                    userLong: thisuser.longitude,
+                                    userLat: thisuser.latitude,
+                                    userBattery: thisuser.battery,
+                                    userModel: thisuser.model,
+                                    userVersion: thisuser.version,
+                                    userAccuracy: thisuser.accuracy,
+                                    userConfidence: thisuser.confidence,
+                                    userActivity: thisuser.activity
+                                };
+//                                var times = {start: record.start, end : record.end};
+//                                extend(true, thisuser, usrObj, times);
+//                                console.log(JSON.stringify(usrObj));
+//                                console.log(JSON.stringify(times));
                                 console.log(JSON.stringify(usrObj));
-                                console.log(JSON.stringify(times));
-                                console.log(JSON.stringify(thisuser));
-                                current.push(thisuser);
+                                current.push(usrObj);
 
                             });
 
@@ -146,7 +172,53 @@ module.exports = function(app) {
             if(err){
                 res.send(500,err);
             }else{
-                console.log("Final count: " + result);
+                var fieldNames = ['Time Stamp',
+                    'Query UserID',
+                    'Longitude',
+                    'Latitude',
+                    'Battery',
+                    'Model',
+                    'Version',
+                    'Accuracy',
+                    'Confidence',
+                    'Activity',
+                    'Contact UserID',
+                    'Longitude',
+                    'Latitude',
+                    'Battery',
+                    'Model',
+                    'Version',
+                    'Accuracy',
+                    'Confidence',
+                    'Activity'];
+                var fields = ['end',
+                    'queryUserId',
+                    'queryUserLong',
+                    'queryUserLat',
+                    'queryUserBattery',
+                    'queryUserModel',
+                    'queryUserVersion',
+                    'queryUserAccuracy',
+                    'queryUserConfidence',
+                    'queryUserActivity',
+                    'userId',
+                    'userLong',
+                    'userLat',
+                    'userBattery',
+                    'userModel',
+                    'userVersion',
+                    'userAccuracy',
+                    'userConfidence',
+                    'userActivity'];
+                json2csv({ data: result, fields: fields, fieldNames: fieldNames, quotes: '' }, function(err, csv) {
+                    if (err) console.log(err);
+                    fs.writeFile('public/file.csv', csv, function(err) {
+                        if (err) throw err;
+                        console.log('file saved');
+                    });
+                });
+
+                console.log("Final count: " + result.length);
                 res.json(result);
             }
         });
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,11 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
 	$scope.tagline = 'To the moon and back!';
+    $scope.people = [];
+    $scope.checked = false;
     $scope.processForm = function() {
+        $scope.people = [];
+        $scope.checked = false;
 //        $http.get('http://localhost:8080/query1', {params: {user: $scope.user, start: $scope.start, end: $scope.end, distance: $scope.distance}}).then(function successCallback(res) {
 //            alert(JSON.stringify(res.data));
 //        }, function errorCallback(res) {
@@ -9,8 +13,12 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
 //
 //        });
 
+
         $http.get('http://localhost:8080/query1', {params: {user: "_User$peyi3uzZdZ", start: "2015-08-07T20:10:47.301Z", end: "2015-08-07T20:50:47.301Z", distance: 10}}).then(function successCallback(res) {
-            alert(JSON.stringify(res.data));
+            console.log(JSON.stringify(res.data));
+            console.log(res.data);
+            $scope.checked = true;
+            $scope.people = res.data;
         }, function errorCallback(res) {
             alert(JSON.stringify(res.data));
 
