27c5b358faa5c6bc287f6bc87a25644de52ca73f
HassebJ <11besemjaved@seecs.edu.pk>
2016-02-26 01:14:11 +0500

    initial working query 2

--- app/routes.js
+++ app/routes.js
@@ -230,11 +230,222 @@ module.exports = function(app) {
 
 
     });
-    // handle things like api calls
-    // authentication routes
 
-    // frontend routes =========================================================
-    // route to handle all angular requests
+    app.get('/query2', function(req, res){
+        console.log("Query:" + JSON.stringify(req.query));
+        async.waterfall([
+
+            function(callback) {
+
+                var diff = 5 * 60 * 1000; //5 minutes
+                var queryEndTimestamp = new Date(req.query.end);
+                var periods = [];
+                var records = [];
+                var start = new Date(req.query.start);
+                var end =new Date(start.getTime() ) ;
+                while(queryEndTimestamp.getTime() >=  start.getTime() + diff ){
+
+                    periods.push({start : new Date(start), end : end, records: records});
+                    end = new Date (end.setTime(end.getTime() + diff));
+                    start = new Date (start.setTime((start.getTime() + diff)));
+                }
+
+                var fieldsToSearch = {}, limit = 1, skip = 0,  sort = '_created_at', sortType= -1;
+                var sortQ = {};
+                sortQ[sort] = sortType;
+
+//                fieldsToSearch._p_user = req.query.user;
+
+                async.forEachOfSeries(periods, function (period, i, foreachcallback) {
+                    fieldsToSearch._created_at = {'$gte': new Date(period.start), '$lt': new Date(period.end)};
+                    var fields = "_p_user longitude latitude battery model version accuracy confidence activity -_id";
+                    var prevData = [];
+
+                    cm_data.find(fieldsToSearch, fields,{
+                            skip: skip,
+                            limit: limit,
+                            sort:sortQ
+                        },
+                        function(err, data){
+                            if (err){
+                                console.log(err);
+                                return foreachcallback(err);
+                            }
+                            if(data.length>0){
+//                                console.log(JSON.stringify(data));
+                                records.push({start: period.start, end:period.end, data: data[0]});
+                            }else{
+                                console.log("No data for this range starting: "+period.start+", using data from previous range.");
+                                records.push({start: period.start, end:period.end, data: prevData[0]});
+                            }
+                            prevData = data;
+
+                            foreachcallback();
+
+
+                        });
+
+                }, function (err) {
+                    if (err)
+                        console.error(err.message);
+                    // configs is now a map of JSON data
+                    callback(null, records);
+                });
+
+            },function(initialusers, callback) {
+                console.log("Initial User: " +JSON.stringify(initialusers));
+
+                var fieldsToSearch = {}, limit = 50, skip = 0,  sort = '_created_at', sortType= -1;
+//                fieldsToSearch._p_user = {'$ne': req.query.user};
+                var results = [];
+                var maxDistance = parseInt(req.query.distance)/6371;
+                var sortQ = {};
+                sortQ[sort] = sortType;
+
+                async.forEachOfSeries(initialusers, function (record, i, foreachcallback) {
+                    var user = record.data;
+                    console.log("user : " + record.data);
+                    var coords = [];
+                    coords[0] = user.longitude;
+                    coords[1] = user.latitude;
+                    fieldsToSearch.loc = {
+                        $near: coords,
+                        $maxDistance: maxDistance
+                    };
+                    fieldsToSearch._created_at = {'$gte': new Date(record.start), '$lt': new Date(record.end)};
+                    var fields = "_p_user longitude latitude battery model version accuracy confidence activity -_id";
+
+
+                    cm_data.find(fieldsToSearch, fields,{
+                            skip: skip,
+                            limit: limit,
+                            sort:sortQ
+                        },
+                        function(err, data){
+                            if (err){
+                                console.log(err);
+                                return foreachcallback(err);
+                            }
+
+//                            console.log(JSON.stringify(data));
+                            var current = [];
+                            data.forEach(function(thisuser){
+                                console.log(JSON.stringify(thisuser));
+                                var usrObj = {
+                                    start: record.start,
+                                    end: record.end,
+                                    queryUserId: user._p_user,
+                                    queryUserLong: user.longitude,
+                                    queryUserLat: user.latitude,
+                                    queryUserBattery: user.battery,
+                                    queryUserModel: user.model,
+                                    queryUserVersion: user.version,
+                                    queryUserAccuracy: user.accuracy,
+                                    queryUserConfidence: user.confidence,
+                                    queryUserActivity: user.activity,
+                                    userId: thisuser._p_user,
+                                    userLong: thisuser.longitude,
+                                    userLat: thisuser.latitude,
+                                    userBattery: thisuser.battery,
+                                    userModel: thisuser.model,
+                                    userVersion: thisuser.version,
+                                    userAccuracy: thisuser.accuracy,
+                                    userConfidence: thisuser.confidence,
+                                    userActivity: thisuser.activity
+                                };
+//                                var times = {start: record.start, end : record.end};
+//                                extend(true, thisuser, usrObj, times);
+//                                console.log(JSON.stringify(usrObj));
+//                                console.log(JSON.stringify(times));
+                                console.log(JSON.stringify(usrObj));
+                                current.push(usrObj);
+
+                            });
+
+
+                            results = results.concat(current);
+
+                            foreachcallback();
+
+
+                        });
+
+                }, function (err) {
+                    if (err)
+                        console.error(err.message);
+                    // configs is now a map of JSON data
+                    callback(null, results);
+                });
+
+            }//,
+//            function(unfilteredusers, callback) {
+//                // arg1 now equals 'three'
+//
+//                callback(null, 'done');
+//            }
+        ], function (err, result) {
+            if(err){
+                res.send(500,err);
+            }else{
+                var fieldNames = ['Time Stamp',
+                    'Query UserID',
+                    'Longitude',
+                    'Latitude',
+                    'Battery',
+                    'Model',
+                    'Version',
+                    'Accuracy',
+                    'Confidence',
+                    'Activity',
+                    'Contact UserID',
+                    'Longitude',
+                    'Latitude',
+                    'Battery',
+                    'Model',
+                    'Version',
+                    'Accuracy',
+                    'Confidence',
+                    'Activity'];
+                var fields = ['end',
+                    'queryUserId',
+                    'queryUserLong',
+                    'queryUserLat',
+                    'queryUserBattery',
+                    'queryUserModel',
+                    'queryUserVersion',
+                    'queryUserAccuracy',
+                    'queryUserConfidence',
+                    'queryUserActivity',
+                    'userId',
+                    'userLong',
+                    'userLat',
+                    'userBattery',
+                    'userModel',
+                    'userVersion',
+                    'userAccuracy',
+                    'userConfidence',
+                    'userActivity'];
+                json2csv({ data: result, fields: fields, fieldNames: fieldNames, quotes: '' }, function(err, csv) {
+                    if (err) console.log(err);
+                    fs.writeFile('public/file2.csv', csv, function(err) {
+                        if (err) throw err;
+                        console.log('file saved');
+                    });
+                });
+
+                console.log("Final count: " + result.length);
+                res.json(result);
+            }
+        });
+
+
+
+
+
+
+
+    });
+
     app.get('*', function(req, res) {
         res.sendfile('./public/index.html');
     });
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,18 +3,12 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
 	$scope.tagline = 'To the moon and back!';
     $scope.people = [];
     $scope.checked = false;
+    $scope.checked2 = false;
     $scope.processForm = function() {
         $scope.people = [];
         $scope.checked = false;
-//        $http.get('http://localhost:8080/query1', {params: {user: $scope.user, start: $scope.start, end: $scope.end, distance: $scope.distance}}).then(function successCallback(res) {
-//            alert(JSON.stringify(res.data));
-//        }, function errorCallback(res) {
-//            alert(JSON.stringify(res.data));
-//
-//        });
 
-
-        $http.get('http://localhost:8080/query1', {params: {user: "_User$peyi3uzZdZ", start: "2015-08-07T20:10:47.301Z", end: "2015-08-07T20:50:47.301Z", distance: 10}}).then(function successCallback(res) {
+        $http.get('http://localhost:8080/query1', {params: {user: "_User$"+$scope.user, start: $scope.start, end: $scope.end, distance: $scope.distance}}).then(function successCallback(res) {
             console.log(JSON.stringify(res.data));
             console.log(res.data);
             $scope.checked = true;
@@ -23,6 +17,42 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
             alert(JSON.stringify(res.data));
 
         });
+
+
+//        $http.get('http://localhost:8080/query1', {params: {user: "_User$peyi3uzZdZ", start: "2015-08-07T20:10:47.301Z", end: "2015-08-07T20:50:47.301Z", distance: 10}}).then(function successCallback(res) {
+//            console.log(JSON.stringify(res.data));
+//            console.log(res.data);
+//            $scope.checked = true;
+//            $scope.people = res.data;
+//        }, function errorCallback(res) {
+//            alert(JSON.stringify(res.data));
+//
+//        });
+    };
+
+    $scope.processForm2 = function() {
+        $scope.people = [];
+        $scope.checked2 = false;
+        $http.get('http://localhost:8080/query2', {params: { start: $scope.start2, end: $scope.end2, distance: $scope.distance2}}).then(function successCallback(res) {
+            console.log(JSON.stringify(res.data));
+            console.log(res.data);
+            $scope.checked2 = true;
+            $scope.people = res.data;
+        }, function errorCallback(res) {
+            alert(JSON.stringify(res.data));
+
+        });
+
+
+//        $http.get('http://localhost:8080/query2', {params: {start: "2015-08-07T20:10:47.301Z", end: "2015-08-07T20:50:47.301Z", distance: 10}}).then(function successCallback(res) {
+//            console.log(JSON.stringify(res.data));
+//            console.log(res.data);
+//            $scope.checked2 = true;
+//            $scope.people = res.data;
+//        }, function errorCallback(res) {
+//            alert(JSON.stringify(res.data));
+//
+//        });
     };
 
 });
