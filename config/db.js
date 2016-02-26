bf33296f8784da451e7b1d59528f702b856c23ee
HassebJ <11besemjaved@seecs.edu.pk>
2016-02-27 03:02:02 +0500

    mongolab url updates

--- config/db.js
+++ config/db.js
@@ -1,3 +1,14 @@
+var mongolaburl = '';
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development'){
+    mongolaburl = 'mongodb://localhost/nodenigeria';
+
+}
+else {
+    console.log(process.env.MONGOURL);
+    mongolaburl = process.env.MONGOURL;
+};
+
 module.exports = {
-	url : 'mongodb://localhost/nodenigeria'
+	url : mongolaburl
 }
